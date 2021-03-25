8a68af1b8f1606132b543fdb8c178f4161c3b002
Mat <mvello66@gmail.com>
2021-03-24 21:34:57 -0400

    adds index method for contacts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h1>{{ message }}</h1>
+    <button v-on:click="contactsIndex">toggle index</button>
+    <div v-for= "contact in contacts" v-if="toggleContacts"> 
+      <p> {{ contact.id }} 
+      {{ contact.first_name }}
+      {{ contact.last_name }}
+      {{ contact.email }}
+      {{ contact.phone_number }}
+      {{ contact.image}}
+      </p>
+
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      test: "testing",
+      contacts: "",
+      toggleContacts: false,
+    };
+  },
+  created: function () {},
+  methods: {
+    contactsIndex: function () {
+      axios.get("http://localhost:3000/api/contacts").then((response) => {
+        console.log(response.data);
+        this.contacts = response.data;
+        this.toggleContacts = !this.toggleContacts;
+      });
+    },
+  },
+};
 </script>
